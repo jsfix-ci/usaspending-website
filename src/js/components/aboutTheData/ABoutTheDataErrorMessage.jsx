20fbcd6417f2ec5241ffe49c4e3830162f1c5f4a
chasls2 <110061741+chasls2@users.noreply.github.com>
2022-11-16 14:40:14 -0600

    added about the data error message

---
+++ src/js/components/aboutTheData/ABoutTheDataErrorMessage.jsx
@@ -0,0 +1,16 @@
+/**
+ * AbouttheDataPage.jsx
+ * Created by Chas Stevens 10/16/2022
+ */
+
+import React from 'react';
+
+const AboutTheDataErrorMessage = () => (
+    <div className="about-the-data__error-message">
+        <p className="about-the-data-paragraph-one">Something went wrong</p>
+        <p className="about-the-data-paragraph-two">Sorry, we're unable to load this content.</p>
+        {/* eslint-disable-next-line no-return-assign */}
+    </div>
+);
+
+export default ErrorWordOfTheDay;
